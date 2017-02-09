b2ad3bf18504a23f8b339273f9a01a6335ad8fe6
Russell Cloran <rcloran@gmail.com>
2017-02-08 20:50:45 -0500

    Answer address requests to broadcast

--- bellows/zigbee/zdo/__init__.py
+++ bellows/zigbee/zdo/__init__.py
@@ -52,7 +52,8 @@ class ZDO:
             if app._ieee == args[0]:
                 self.reply(0x8000, 0, app._ieee, app._nwk, 0, 0, [])
         if command_id == 0x0001:  # IEEE_addr_req
-            if app._nwk == args[0]:
+            broadcast = (0xffff, 0xfffd, 0xfffc)
+            if args[0] in broadcast or app._nwk == args[0]:
                 self.reply(0x8001, 0, app._ieee, app._nwk, 0, 0, [])
         elif command_id == 0x0006:  # Match_Desc_req
             self.handle_match_desc(*args)
