0c2b81e4e4466a3fd6ebe974a6d81c64537c974d
Russell Cloran <rcloran@gmail.com>
2017-03-08 21:20:27 -0800

    0.2.2 -> 0.2.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="bellows",
-    version="0.2.2",
+    version="0.2.3",
     description="",
     url="http://github.com/rcloran/bellows",
     author="Russell Cloran",
