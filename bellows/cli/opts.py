7dd936119ec1e3a2bca27dc2fba9df3ef628a994
Gavin Mogan <git@gavinmogan.com>
2017-10-08 00:48:00 -0700

    Add error reporting when database isn't provided when needed
    New output:
    ```
     halkeye@odin  ~/git/bellows  EZSP_DEVICE=/dev/zigbee bellows devices
     Usage: bellows devices [OPTIONS]
    
     Error: Invalid value for "-D" / "--database": Path
     "/home/halkeye/.config/bellows/app.db" does not exist.
    ```

--- bellows/cli/opts.py
+++ bellows/cli/opts.py
@@ -38,10 +38,13 @@ baudrate = click.option(
 database_file = click.option(
     '-D', '--database',
     type=click.Path(
+        exists=True,
         dir_okay=False,
         writable=True,
     ),
+    required=True,
     default=os.path.join(click.get_app_dir("bellows"), "app.db"),
+
 )
 
 duration_ms = click.option(
