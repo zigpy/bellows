c8a9c58d402245df3c2f230b78afa6532e10b2b4
Alexei Chetroi <lexoid@gmail.com>
2019-05-04 22:29:22 -0400

    0.7.2 -> 0.7.3 (#160)

---
+++ bellows/__init__.py
@@ -0,0 +1,5 @@
+MAJOR_VERSION = 0
+MINOR_VERSION = 7
+PATCH_VERSION = '3'
+__short_version__ = '{}.{}'.format(MAJOR_VERSION, MINOR_VERSION)
+__version__ = '{}.{}'.format(__short_version__, PATCH_VERSION)
