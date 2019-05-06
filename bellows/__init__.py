0b211f949e09d43be8bb1070d702ef1140a96b37
Alexei Chetroi <lexoid@gmail.com>
2019-05-06 11:51:27 -0400

    version bump to 0.7.4.dev0

--- bellows/__init__.py
+++ bellows/__init__.py
@@ -1,5 +1,5 @@
 MAJOR_VERSION = 0
 MINOR_VERSION = 7
-PATCH_VERSION = '3'
+PATCH_VERSION = '4.dev0'
 __short_version__ = '{}.{}'.format(MAJOR_VERSION, MINOR_VERSION)
 __version__ = '{}.{}'.format(__short_version__, PATCH_VERSION)
