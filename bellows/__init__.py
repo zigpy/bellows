b8377cdea0bf4a23256788a09897cae2cbfbb937
Alexei Chetroi <lexoid@gmail.com>
2020-03-31 22:14:46 -0400

    0.15.1 version bump

--- bellows/__init__.py
+++ bellows/__init__.py
@@ -1,5 +1,5 @@
 MAJOR_VERSION = 0
-MINOR_VERSION = 16
-PATCH_VERSION = "0.dev0"
+MINOR_VERSION = 15
+PATCH_VERSION = "1"
 __short_version__ = "{}.{}".format(MAJOR_VERSION, MINOR_VERSION)
 __version__ = "{}.{}".format(__short_version__, PATCH_VERSION)
