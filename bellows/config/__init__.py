069cea3e2b4455992f06f71a5c9d2a1195534f8b
Alexei Chetroi <lexoid@gmail.com>
2020-04-12 13:46:54 -0400

    Configure EZSP based on EZSP schema (#236)
    * Add EZSP config schema.
    * Configure EZSP based on schema defaults.

--- bellows/config/__init__.py
+++ bellows/config/__init__.py
@@ -7,8 +7,11 @@ from zigpy.config import (  # noqa: F401 pylint: disable=unused-import
     cv_boolean,
 )
 
-CONF_PARAM_SRC_RTG = "source_routing"
+from .ezsp import EZSP_SCHEMA
+
 CONF_DEVICE_BAUDRATE = "baudrate"
+CONF_EZSP_CONFIG = "ezsp_config"
+CONF_PARAM_SRC_RTG = "source_routing"
 
 SCHEMA_DEVICE = SCHEMA_DEVICE.extend(
     {vol.Optional(CONF_DEVICE_BAUDRATE, default=57600): int}
@@ -18,5 +21,6 @@ CONFIG_SCHEMA = CONFIG_SCHEMA.extend(
     {
         vol.Required(CONF_DEVICE): SCHEMA_DEVICE,
         vol.Optional(CONF_PARAM_SRC_RTG, default=False): cv_boolean,
+        vol.Optional(CONF_EZSP_CONFIG, default={}): vol.Schema(EZSP_SCHEMA),
     }
 )
