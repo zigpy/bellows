2f51de765d8837b2b9d50812c1ae8dd555d957a4
Alexei Chetroi <alexei.chetroi@outlook.com>
2020-07-24 17:33:28 -0400

    Remove correct config keys.

--- bellows/ezsp/v5/config.py
+++ bellows/ezsp/v5/config.py
@@ -1,9 +1,6 @@
 from ..v4 import config as v4_config
 
-_deletions = (
-    "EZSP_CONFIG_BROADCAST_ALARM_DATA_SIZE",
-    "EZSP_CONFIG_UNICAST_ALARM_DATA_SIZE",
-)
+_deletions = ("CONFIG_BROADCAST_ALARM_DATA_SIZE", "CONFIG_UNICAST_ALARM_DATA_SIZE")
 
 EZSP_SCHEMA = {k: v for k, v in v4_config.EZSP_SCHEMA.items() if k not in _deletions}
 
