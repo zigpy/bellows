2207a051d4c06c457b36cd71d6344cb7a549ee73
Alexei Chetroi <alexei.chetroi@outlook.com>
2020-08-29 21:02:24 -0400

    Update application module coverage

--- bellows/zigbee/application.py
+++ bellows/zigbee/application.py
@@ -519,7 +519,7 @@ class ControllerApplication(zigpy.application.ControllerApplication):
         wild_card_ieee = t.EmberEUI64([0xFF] * 8)
         tc_link_key = t.EmberKeyData(b"ZigBeeAlliance09")
         await self._ezsp.addTransientLinkKey(wild_card_ieee, tc_link_key)
-        asyncio.create_task(self._ezsp.pre_permit(time_s))
+        await self._ezsp.pre_permit(time_s)
         await super().permit(time_s, node)
 
     def permit_ncp(self, time_s=60):
@@ -722,4 +722,8 @@ class EZSPCoordinator(CustomDevice):
         }
     }
 
-    replacement = {"endpoints": {1: (EZSPEndpoint, {})}}
+    replacement = {
+        "endpoints": {1: (EZSPEndpoint, {})},
+        "manufacturer": "Silicon Labs",
+        "model": "EZSP",
+    }
