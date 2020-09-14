a0ac1b3aba1c6bc9b7b837bd62e4f2b466f73439
Alexei Chetroi <lexoid@gmail.com>
2020-09-14 17:56:42 -0400

    Bump up zigpy dependency

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         "pure_pcapy3==1.0.1",
         "pyserial-asyncio",
         "voluptuous",
-        "zigpy>=0.20.1a3",
+        "zigpy>=0.21.0",
     ],
     dependency_links=["https://codeload.github.com/rcloran/pure-pcapy-3/zip/master"],
     tests_require=["asynctest", "pytest", "pytest-asyncio"],
