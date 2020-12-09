032a4759f913475120121f96c288f7b31ed8962a
Alexei Chetroi <alexei.chetroi@outlook.com>
2020-12-09 10:45:50 -0500

    Clear EZSP counters every 30min

--- bellows/zigbee/application.py
+++ bellows/zigbee/application.py
@@ -28,6 +28,7 @@ import bellows.zigbee.util
 APS_ACK_TIMEOUT = 120
 EZSP_DEFAULT_RADIUS = 0
 EZSP_MULTICAST_NON_MEMBER_RADIUS = 3
+EZSP_COUNTER_CLEAR_INTERVAL = 180  # Clear counters every n * WATCHDOG_WAKE_PERIOD
 MAX_WATCHDOG_FAILURES = 4
 RESET_ATTEMPT_BACKOFF_TIME = 5
 WATCHDOG_WAKE_PERIOD = 10
@@ -620,6 +621,8 @@ class ControllerApplication(zigpy.application.ControllerApplication):
         """Watchdog handler."""
         LOGGER.debug("Starting EZSP watchdog")
         failures = 0
+        read_counter = 0
+
         await asyncio.sleep(WATCHDOG_WAKE_PERIOD)
         while True:
             try:
@@ -629,7 +632,12 @@ class ControllerApplication(zigpy.application.ControllerApplication):
                 if LOGGER.level < logging.DEBUG or self._ezsp.ezsp_version == 4:
                     await self._ezsp.nop()
                 else:
-                    (res,) = await self._ezsp.readCounters()
+                    read_counter = (read_counter + 1) % EZSP_COUNTER_CLEAR_INTERVAL
+                    if read_counter:
+                        operation = self._ezsp.readCounters
+                    else:
+                        operation = self._ezsp.readAndClearCounters
+                    (res,) = await operation()
                     counters = (
                         f"{counter.name}: {value}"
                         for counter, value in zip(
