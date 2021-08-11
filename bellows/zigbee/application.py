2822b63e45ed500a1349bf2498e9c23c5cebb07d
Alexei Chetroi <Alexei.Chetroi@outlook.com>
2021-08-10 20:35:17 -0400

    Set route discovery if no relays

--- bellows/zigbee/application.py
+++ bellows/zigbee/application.py
@@ -512,8 +512,15 @@ class ControllerApplication(zigpy.application.ControllerApplication):
             async with self._in_flight_msg:
                 delays = [0.5, 1.0, 1.5]
                 while True:
-                    if self.use_source_routing and self._ezsp.ezsp_version < 8:
-                        (res,) = await self._ezsp.set_source_route(device)
+                    if self.use_source_routing:
+                        if self._ezsp.ezsp_version < 8:
+                            (res,) = await self._ezsp.set_source_route(device)
+                        else:
+                            res = (
+                                t.EmberStatus.SUCCESS
+                                if device.relays is not None
+                                else t.EmberStatus.ERR_FATAL
+                            )
                         if res != t.EmberStatus.SUCCESS:
                             aps_frame.options ^= (
                                 t.EmberApsOption.APS_OPTION_ENABLE_ROUTE_DISCOVERY
