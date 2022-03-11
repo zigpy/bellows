25b0bac81047d363e2126d0c4f1d70a05f341888
Sylvain PERON <48243214+SylvainPer@users.noreply.github.com>
2022-03-11 14:12:52 +0100

    Import CONF_NWK parameters to config (#451)
    Allows network configuration at initialization

--- bellows/config/__init__.py
+++ bellows/config/__init__.py
@@ -4,6 +4,15 @@ from zigpy.config import (  # noqa: F401 pylint: disable=unused-import
     CONF_DEVICE_PATH,
     CONFIG_SCHEMA,
     SCHEMA_DEVICE,
+    CONF_NWK,
+    CONF_NWK_KEY,
+    CONF_NWK_PAN_ID,
+    CONF_NWK_CHANNEL,
+    CONF_NWK_CHANNELS,
+    CONF_NWK_UPDATE_ID,
+    CONF_NWK_TC_ADDRESS,
+    CONF_NWK_TC_LINK_KEY,
+    CONF_NWK_EXTENDED_PAN_ID,
     cv_boolean,
 )
 
