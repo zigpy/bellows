846545401376abf96c9862c0b7ae7ce184e298c3
puddly <32534428+puddly@users.noreply.github.com>
2022-05-05 12:06:27 -0400

    Only use one pair of `network_info` and `node_info` fixtures

--- tests/test_util.py
+++ tests/test_util.py
@@ -7,57 +7,19 @@ import bellows.types as bellows_t
 import bellows.zigbee.util as util
 
 from tests.test_application import ezsp_mock
+from tests.test_application_network_state import network_info, node_info
 
 
 @pytest.fixture
-def node_info():
-    return zigpy.state.NodeInfo(
-        nwk=t.NWK(0x0000),
-        ieee=t.EUI64.convert("93:2C:A9:34:D9:D0:5D:12"),
-        logical_type=zdo_t.LogicalType.Coordinator,
-    )
-
-
-@pytest.fixture
-def network_info(node_info):
-    return zigpy.state.NetworkInfo(
-        extended_pan_id=t.ExtendedPanId.convert("0D:49:91:99:AE:CD:3C:35"),
-        pan_id=t.PanId(0x9BB0),
-        nwk_update_id=0x12,
-        nwk_manager_id=t.NWK(0x0000),
-        channel=t.uint8_t(15),
-        channel_mask=t.Channels.from_channel_list([15, 20, 25]),
-        security_level=t.uint8_t(5),
-        network_key=zigpy.state.Key(
-            key=t.KeyData.convert("9A:79:D6:9A:DA:EC:45:C6:F2:EF:EB:AF:DA:A3:07:B6"),
-            seq=108,
-            tx_counter=39009277,
-        ),
-        tc_link_key=zigpy.state.Key(
-            key=t.KeyData(b"ZigBeeAlliance09"),
-            partner_ieee=node_info.ieee,
-            tx_counter=8712428,
-        ),
-        key_table=[],
-        children=[],
-        nwk_addresses={},
-        stack_specific={"ezsp": {"hashed_tclk": "71e31105bb92a2d15747a0d0a042dbfd"}},
-    )
-
-
-@pytest.fixture
-def zigpy_key():
-    return zigpy.state.Key(
-        key=t.KeyData.convert("9A:79:D6:9A:DA:EC:45:C6:F2:EF:EB:AF:DA:A3:07:B6"),
-        seq=108,
-        tx_counter=39009277,
+def zigpy_key(network_info, node_info):
+    return network_info.network_key.replace(
         rx_counter=1234567,
-        partner_ieee=t.EUI64.convert("0D:49:91:99:AE:CD:3C:35"),
+        partner_ieee=node_info.ieee,
     )
 
 
 @pytest.fixture
-def ezsp_key(ezsp_mock):
+def ezsp_key(ezsp_mock, network_info, node_info, zigpy_key):
     ezsp = ezsp_mock
     return ezsp.types.EmberKeyStruct(
         bitmask=(
@@ -66,11 +28,11 @@ def ezsp_key(ezsp_mock):
             | ezsp.types.EmberKeyStructBitmask.KEY_HAS_INCOMING_FRAME_COUNTER
             | ezsp.types.EmberKeyStructBitmask.KEY_HAS_PARTNER_EUI64
         ),
-        key=ezsp.types.EmberKeyData(bytes.fromhex("9A79D69ADAEC45C6F2EFEBAFDAA307B6")),
-        sequenceNumber=108,
-        outgoingFrameCounter=39009277,
-        incomingFrameCounter=1234567,
-        partnerEUI64=bellows_t.EmberEUI64.convert("0D:49:91:99:AE:CD:3C:35"),
+        key=ezsp.types.EmberKeyData(network_info.network_key.key),
+        sequenceNumber=zigpy_key.seq,
+        outgoingFrameCounter=zigpy_key.tx_counter,
+        incomingFrameCounter=zigpy_key.rx_counter,
+        partnerEUI64=bellows_t.EmberEUI64(node_info.ieee),
     )
 
 
@@ -83,8 +45,8 @@ def test_zha_security_normal(network_info, node_info):
         security.preconfiguredTrustCenterEui64 == network_info.tc_link_key.partner_ieee
     )
     assert (
-        security.bitmask & bellows_t.EmberInitialSecurityBitmask.HAVE_TRUST_CENTER_EUI64
-        == bellows_t.EmberInitialSecurityBitmask.HAVE_TRUST_CENTER_EUI64
+        bellows_t.EmberInitialSecurityBitmask.HAVE_TRUST_CENTER_EUI64
+        in security.bitmask
     )
 
     assert (
@@ -92,9 +54,8 @@ def test_zha_security_normal(network_info, node_info):
         == network_info.stack_specific["ezsp"]["hashed_tclk"]
     )
     assert (
-        security.bitmask
-        & bellows_t.EmberInitialSecurityBitmask.TRUST_CENTER_USES_HASHED_LINK_KEY
-        == bellows_t.EmberInitialSecurityBitmask.TRUST_CENTER_USES_HASHED_LINK_KEY
+        bellows_t.EmberInitialSecurityBitmask.TRUST_CENTER_USES_HASHED_LINK_KEY
+        in security.bitmask
     )
 
 
@@ -106,15 +67,14 @@ def test_zha_security_router(network_info, node_info):
 
     assert security.preconfiguredTrustCenterEui64 == bellows_t.EmberEUI64([0x00] * 8)
     assert (
-        security.bitmask & bellows_t.EmberInitialSecurityBitmask.HAVE_TRUST_CENTER_EUI64
-        != bellows_t.EmberInitialSecurityBitmask.HAVE_TRUST_CENTER_EUI64
+        bellows_t.EmberInitialSecurityBitmask.HAVE_TRUST_CENTER_EUI64
+        not in security.bitmask
     )
 
     assert security.preconfiguredKey == network_info.tc_link_key.key
     assert (
-        security.bitmask
-        & bellows_t.EmberInitialSecurityBitmask.TRUST_CENTER_USES_HASHED_LINK_KEY
-        != bellows_t.EmberInitialSecurityBitmask.TRUST_CENTER_USES_HASHED_LINK_KEY
+        bellows_t.EmberInitialSecurityBitmask.TRUST_CENTER_USES_HASHED_LINK_KEY
+        not in security.bitmask
     )
 
 
