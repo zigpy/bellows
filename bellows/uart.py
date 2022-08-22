971b4dbaada2b1e2777c8009e4e6dd9addd796cb
Stefan Agner <stefan@agner.ch>
2022-08-22 18:48:09 +0200

    Add support for tcp protocol
    Add tcp:// protocol to talk to EZSP radio via TCP/IP. Compared to the
    existing socket:// protocol this uses Python's loop.create_connection()
    instead of pyserial. In tests this makes `bellows info` to complete
    about 50x faster (measured after reset, from 0.6s to 0.012s).

--- bellows/uart.py
+++ bellows/uart.py
@@ -4,6 +4,7 @@ import logging
 
 import serial
 import serial_asyncio
+import urllib.parse
 
 from bellows.config import (
     CONF_DEVICE_BAUDRATE,
@@ -359,16 +360,22 @@ async def _connect(config, application):
     else:
         xon_xoff, rtscts = False, True
 
-    transport, protocol = await serial_asyncio.create_serial_connection(
-        loop,
-        lambda: protocol,
-        url=config[CONF_DEVICE_PATH],
-        baudrate=config[CONF_DEVICE_BAUDRATE],
-        parity=serial.PARITY_NONE,
-        stopbits=serial.STOPBITS_ONE,
-        xonxoff=xon_xoff,
-        rtscts=rtscts,
-    )
+    parsed_path = urllib.parse.urlparse(config[CONF_DEVICE_PATH])
+    if parsed_path.scheme == "socket":
+        transport, protocol = await loop.create_connection(
+            lambda: protocol, parsed_path.hostname, parsed_path.port
+        )
+    else:
+        transport, protocol = await serial_asyncio.create_serial_connection(
+            loop,
+            lambda: protocol,
+            url=config[CONF_DEVICE_PATH],
+            baudrate=config[CONF_DEVICE_BAUDRATE],
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            xonxoff=xon_xoff,
+            rtscts=rtscts,
+        )
 
     await connection_future
 
