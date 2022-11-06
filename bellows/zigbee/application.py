zigpy/bellows

commit 485e58571d86c269e214dfd2c783f6152b7e45ac

make ACK dependents on packet.tx_options (#505)
To prevent send_packet to always set APS_OPTION_RETRY check if packet tx_options requires or not ACK.
Addressing #504

bellows/zigbee/application.py
@@ -708,7 +708,11 @@ class ControllerApplication(zigpy.application.ControllerApplication):
         aps_frame.clusterId = t.uint16_t(packet.cluster_id)
         aps_frame.sourceEndpoint = t.uint8_t(packet.src_ep)
         aps_frame.destinationEndpoint = t.uint8_t(packet.dst_ep or 0)
-        aps_frame.options = t.EmberApsOption.APS_OPTION_RETRY
+        aps_frame.options = t.EmberApsOption.APS_OPTION_NONE
+        
+        if zigpy.types.TransmitOptions.ACK in packet.tx_options:
+            aps_frame.options = t.EmberApsOption.APS_OPTION_RETRY
+
         aps_frame.sequence = t.uint8_t(packet.tsn)
 
         if packet.dst.addr_mode == zigpy.types.AddrMode.Group:

commit 86aebb7fa3999c75a7ba78674409930ed2f3e1ae

Fix CI failures caused by #505

bellows/zigbee/application.py
@@ -704,16 +704,15 @@ class ControllerApplication(zigpy.application.ControllerApplication):
             )
 
         aps_frame = t.EmberApsFrame()
+        aps_frame.sequence = t.uint8_t(packet.tsn)
         aps_frame.profileId = t.uint16_t(packet.profile_id)
         aps_frame.clusterId = t.uint16_t(packet.cluster_id)
         aps_frame.sourceEndpoint = t.uint8_t(packet.src_ep)
         aps_frame.destinationEndpoint = t.uint8_t(packet.dst_ep or 0)
         aps_frame.options = t.EmberApsOption.APS_OPTION_NONE
-        
-        if zigpy.types.TransmitOptions.ACK in packet.tx_options:
-            aps_frame.options = t.EmberApsOption.APS_OPTION_RETRY
 
-        aps_frame.sequence = t.uint8_t(packet.tsn)
+        if zigpy.types.TransmitOptions.ACK in packet.tx_options:
+            aps_frame.options |= t.EmberApsOption.APS_OPTION_RETRY
 
         if packet.dst.addr_mode == zigpy.types.AddrMode.Group:
             aps_frame.groupId = t.uint16_t(packet.dst.address)