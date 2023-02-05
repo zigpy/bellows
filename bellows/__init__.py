66a8adea008801eb33fbb6ebe493bd7c9f9291f9
puddly <32534428+puddly@users.noreply.github.com>
2023-02-05 12:44:48 -0500

    0.34.8 version bump

--- bellows/__init__.py
+++ bellows/__init__.py
@@ -1,5 +1,5 @@
 MAJOR_VERSION = 0
-MINOR_VERSION = 35
-PATCH_VERSION = "0.dev0"
+MINOR_VERSION = 34
+PATCH_VERSION = "8"
 __short_version__ = f"{MAJOR_VERSION}.{MINOR_VERSION}"
 __version__ = f"{__short_version__}.{PATCH_VERSION}"
