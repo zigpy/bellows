e97259af66160b4f676cef793f179016cd588165
puddly <32534428+puddly@users.noreply.github.com>
2023-03-28 15:01:44 -0400

    Inherit types and definition from EZSP v10

--- bellows/ezsp/v11/config.py
+++ bellows/ezsp/v11/config.py
@@ -3,11 +3,11 @@ import voluptuous as vol
 from bellows.config import cv_uint16
 
 from ..v4.config import EZSP_POLICIES_SHARED
-from ..v9 import config as v9_config
+from ..v10 import config as v10_config
 from .types import EzspPolicyId
 
 EZSP_SCHEMA = {
-    **v9_config.EZSP_SCHEMA,
+    **v10_config.EZSP_SCHEMA,
 }
 
 EZSP_POLICIES_SCH = {
---
+++ bellows/ezsp/v11/types.py
@@ -0,0 +1,1 @@
+from ..v10.types import *  # noqa: F401, F403
