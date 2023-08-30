zigpy/bellows

commit 21cd029d9de48fc84e6882877abf6d7ce35b7ede

Repair incorrect TCLK partner IEEE address on startup (#577)
* Always write TCLK's partner IEEE

* Do not propagate unwritten EUI64s when writing network info

* Reset the custom EUI64 when resetting the network

* Rewrite the trust center address in NVRAM

* Overwrite the network info's device IEEE address when it isn't written

* Migrate all protocols to minimal inheritance

* Reorganize initialization logic to allow for simpler startup reset

* Remove unnecessary command from EZSP protocol

* Fix existing unit tests

* Increase coverage of old code back to 100%

* Unit test new methods

* Unit test new application `connect` method

* Unit test call to `repair_tclk_partner_ieee`

* Increase project coverage so PR can be merged

bellows/zigbee/application.py
@@ -25,7 +25,9 @@ import zigpy.zdo.types as zdo_t
 
 import bellows
 from bellows.config import (
+    CONF_EZSP_POLICIES,
     CONF_PARAM_MAX_WATCHDOG_FAILURES,
+    CONF_USE_THREAD,
     CONFIG_SCHEMA,
     SCHEMA_DEVICE,
 )
@@ -34,6 +36,7 @@ import bellows.ezsp
 from bellows.ezsp.v8.types.named import EmberDeviceUpdate
 import bellows.multicast
 import bellows.types as t
+from bellows.zigbee import repairs
 from bellows.zigbee.device import EZSPEndpoint
 import bellows.zigbee.util as util
 
@@ -129,17 +132,17 @@ class ControllerApplication(zigpy.application.ControllerApplication):
 
         return None, None, None
 
-    async def connect(self):
-        self._ezsp = await bellows.ezsp.EZSP.initialize(self.config)
-        ezsp = self._ezsp
+    async def connect(self) -> None:
+        ezsp = bellows.ezsp.EZSP(self.config[zigpy.config.CONF_DEVICE])
+        await ezsp.connect(use_thread=self.config[CONF_USE_THREAD])
 
-        self._multicast = bellows.multicast.Multicast(ezsp)
-        await self.register_endpoints()
+        try:
+            await ezsp.startup_reset()
+        except Exception:
+            ezsp.close()
+            raise
 
-        brd_manuf, brd_name, version = await self._get_board_info()
-        LOGGER.info("EZSP Radio manufacturer: %s", brd_manuf)
-        LOGGER.info("EZSP Radio board name: %s", brd_name)
-        LOGGER.info("EmberZNet version: %s", version)
+        self._ezsp = ezsp
 
     async def _ensure_network_running(self) -> bool:
         """Ensures the network is currently running and returns whether or not the network
@@ -168,7 +171,16 @@ class ControllerApplication(zigpy.application.ControllerApplication):
 
         await self._ensure_network_running()
 
-        await ezsp.update_policies(self.config)
+        if await repairs.fix_invalid_tclk_partner_ieee(ezsp):
+            # Reboot the stack after modifying NV3
+            ezsp.stop_ezsp()
+            await ezsp.startup_reset()
+            await self._ensure_network_running()
+
+        if self.config[zigpy.config.CONF_SOURCE_ROUTING]:
+            await ezsp.set_source_routing()
+
+        await ezsp._protocol.update_policies(self.config[CONF_EZSP_POLICIES])
         await self.load_network_info(load_devices=False)
 
         for cnt_group in self.state.counters:
@@ -206,7 +218,8 @@ class ControllerApplication(zigpy.application.ControllerApplication):
         if db_device is not None and 1 in db_device.endpoints:
             ezsp_device.endpoints[1].member_of.update(db_device.endpoints[1].member_of)
 
-        await self.multicast.startup(ezsp_device)
+        self._multicast = bellows.multicast.Multicast(ezsp)
+        await self._multicast.startup(ezsp_device)
 
     async def load_network_info(self, *, load_devices=False) -> None:
         ezsp = self._ezsp
@@ -353,6 +366,7 @@ class ControllerApplication(zigpy.application.ControllerApplication):
 
         stack_specific = network_info.stack_specific.get("ezsp", {})
         (current_eui64,) = await ezsp.getEui64()
+        wrote_eui64 = False
 
         if (
             node_info.ieee != zigpy.types.EUI64.UNKNOWN
@@ -360,6 +374,7 @@ class ControllerApplication(zigpy.application.ControllerApplication):
         ):
             if await ezsp.can_rewrite_custom_eui64():
                 await ezsp.write_custom_eui64(node_info.ieee)
+                wrote_eui64 = True
             elif not stack_specific.get(
                 "i_understand_i_can_update_eui64_only_once_and_i_still_want_to_do_it"
             ):
@@ -375,6 +390,13 @@ class ControllerApplication(zigpy.application.ControllerApplication):
                 )
             else:
                 await ezsp.write_custom_eui64(node_info.ieee, burn_into_userdata=True)
+                wrote_eui64 = True
+
+        # If we cannot write the new EUI64, don't mess up key entries with the unwritten
+        # EUI64 address
+        if not wrote_eui64:
+            node_info.ieee = current_eui64
+            network_info.tc_link_key.partner_ieee = current_eui64
 
         use_hashed_tclk = ezsp.ezsp_version > 4
 
@@ -386,7 +408,6 @@ class ControllerApplication(zigpy.application.ControllerApplication):
 
         initial_security_state = util.zha_security(
             network_info=network_info,
-            node_info=node_info,
             use_hashed_tclk=use_hashed_tclk,
         )
         (status,) = await ezsp.setInitialSecurityState(initial_security_state)
@@ -443,6 +464,9 @@ class ControllerApplication(zigpy.application.ControllerApplication):
         # Clear the key table
         (status,) = await self._ezsp.clearKeyTable()
         assert status == t.EmberStatus.SUCCESS
+
+        # Reset the custom EUI64
+        await self._ezsp.reset_custom_eui64()
 
     async def disconnect(self):
         # TODO: how do you shut down the stack?

tests/test_application.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+from unittest.mock import AsyncMock, MagicMock, PropertyMock, patch, sentinel
 
 import pytest
 import zigpy.config
@@ -19,10 +20,9 @@ import bellows.ezsp.v8.types as ezsp_t8
 import bellows.types.struct
 import bellows.uart as uart
 import bellows.zigbee.application
+from bellows.zigbee.application import ControllerApplication
 import bellows.zigbee.device
 from bellows.zigbee.util import map_rssi_to_energy
-
-from .async_mock import AsyncMock, MagicMock, PropertyMock, patch, sentinel
 
 APP_CONFIG = {
     config.CONF_DEVICE: {
@@ -34,11 +34,31 @@ APP_CONFIG = {
 
 
 @pytest.fixture
-def ezsp_mock():
+def ieee(init=0):
+    return t.EmberEUI64(map(t.uint8_t, range(init, init + 8)))
+
+
+@pytest.fixture
+def ezsp_mock(ieee):
     """EZSP fixture"""
     mock_ezsp = MagicMock(spec=ezsp.EZSP)
     mock_ezsp.ezsp_version = 7
     mock_ezsp.setManufacturerCode = AsyncMock()
+    mock_ezsp.getEui64 = AsyncMock(return_value=[ieee])
+    mock_ezsp.getConfigurationValue = AsyncMock(return_value=[t.EmberStatus.SUCCESS, 0])
+    mock_ezsp.getCurrentSecurityState = AsyncMock(
+        return_value=[
+            t.EmberStatus.SUCCESS,
+            t.EmberCurrentSecurityState(
+                bitmask=(
+                    t.EmberCurrentSecurityBitmask.GLOBAL_LINK_KEY
+                    | t.EmberCurrentSecurityBitmask.HAVE_TRUST_CENTER_LINK_KEY
+                    | 224
+                ),
+                trustCenterLongAddress=ieee,
+            ),
+        ]
+    )
     mock_ezsp.set_source_route = AsyncMock(return_value=[t.EmberStatus.SUCCESS])
     mock_ezsp.addTransientLinkKey = AsyncMock(return_value=[0])
     mock_ezsp.readCounters = AsyncMock(return_value=[[0] * 10])
@@ -50,6 +70,7 @@ def ezsp_mock():
     mock_ezsp.wait_for_stack_status.return_value.__enter__ = AsyncMock(
         return_value=t.EmberStatus.NETWORK_UP
     )
+    mock_ezsp._protocol = AsyncMock()
 
     type(mock_ezsp).types = ezsp_t7
     type(mock_ezsp).is_ezsp_running = PropertyMock(return_value=True)
@@ -60,10 +81,8 @@ def ezsp_mock():
 @pytest.fixture
 def make_app(monkeypatch, event_loop, ezsp_mock):
     def inner(config):
-        app_cfg = bellows.zigbee.application.ControllerApplication.SCHEMA(
-            {**APP_CONFIG, **config}
-        )
-        app = bellows.zigbee.application.ControllerApplication(app_cfg)
+        app_cfg = ControllerApplication.SCHEMA({**APP_CONFIG, **config})
+        app = ControllerApplication(app_cfg)
 
         app._ezsp = ezsp_mock
         monkeypatch.setattr(bellows.zigbee.application, "APS_ACK_TIMEOUT", 0.05)
@@ -91,11 +110,6 @@ def aps():
     f.destinationEndpoint = 8
     f.sequence = 100
     return f
-
-
-@pytest.fixture
-def ieee(init=0):
-    return t.EmberEUI64(map(t.uint8_t, range(init, init + 8)))
 
 
 @patch("zigpy.device.Device._initialize", new=AsyncMock())
@@ -126,12 +140,14 @@ async def _test_startup(
         return [t.EmberStatus.NETWORK_DOWN]
 
     app._in_flight_msg = None
-    ezsp_mock = MagicMock()
+    ezsp_mock = MagicMock(spec=ezsp.EZSP)
     ezsp_mock.types = ezsp_t7
     type(ezsp_mock).ezsp_version = PropertyMock(return_value=ezsp_version)
     ezsp_mock.initialize = AsyncMock(return_value=ezsp_mock)
     ezsp_mock.connect = AsyncMock()
+    ezsp_mock._protocol = AsyncMock()
     ezsp_mock.setConcentrator = AsyncMock()
+    ezsp_mock.getTokenData = AsyncMock(return_value=[t.EmberStatus.ERR_FATAL, b""])
     ezsp_mock._command = AsyncMock(return_value=t.EmberStatus.SUCCESS)
     ezsp_mock.addEndpoint = AsyncMock(return_value=t.EmberStatus.SUCCESS)
     ezsp_mock.setConfigurationValue = AsyncMock(return_value=t.EmberStatus.SUCCESS)
@@ -179,12 +195,15 @@ async def _test_startup(
         return_value=[
             t.EmberStatus.SUCCESS,
             t.EmberCurrentSecurityState(
-                bitmask=t.EmberCurrentSecurityBitmask.TRUST_CENTER_USES_HASHED_LINK_KEY,
-                trustCenterLongAddress=t.EmberEUI64.convert("ff:ff:ff:ff:ff:ff:ff:ff"),
+                bitmask=(
+                    t.EmberCurrentSecurityBitmask.GLOBAL_LINK_KEY
+                    | t.EmberCurrentSecurityBitmask.HAVE_TRUST_CENTER_LINK_KEY
+                    | 224
+                ),
+                trustCenterLongAddress=ieee,
             ),
         ]
     )
-    ezsp_mock.pre_permit = AsyncMock()
     app.permit = AsyncMock()
 
     def form_network():
@@ -196,7 +215,7 @@ async def _test_startup(
 
     app.form_network = AsyncMock(side_effect=form_network)
 
-    p1 = patch.object(bellows.ezsp, "EZSP", new=ezsp_mock)
+    p1 = patch("bellows.ezsp.EZSP", return_value=ezsp_mock)
     p2 = patch.object(bellows.multicast.Multicast, "startup")
 
     with p1, p2 as multicast_mock:
@@ -1641,8 +1660,6 @@ async def test_startup_coordinator_existing_groups_joined(app, ieee):
     app._ensure_network_running = AsyncMock()
     app._ezsp.update_policies = AsyncMock()
     app.load_network_info = AsyncMock()
-
-    app._multicast = bellows.multicast.Multicast(app._ezsp)
     app.state.node_info.ieee = ieee
 
     db_device = app.add_device(ieee, 0x0000)
@@ -1666,8 +1683,6 @@ async def test_startup_new_coordinator_no_groups_joined(app, ieee):
     app._ensure_network_running = AsyncMock()
     app._ezsp.update_policies = AsyncMock()
     app.load_network_info = AsyncMock()
-
-    app._multicast = bellows.multicast.Multicast(app._ezsp)
     app.state.node_info.ieee = ieee
 
     p1 = patch.object(bellows.multicast.Multicast, "_initialize")
@@ -1691,9 +1706,6 @@ async def test_startup_source_routing(make_app, ieee, enable_source_routing):
     app._ensure_network_running = AsyncMock()
     app.load_network_info = AsyncMock()
     app.state.node_info.ieee = ieee
-
-    app._multicast = bellows.multicast.Multicast(app._ezsp)
-    app._multicast._initialize = AsyncMock()
 
     mock_device = MagicMock()
     mock_device.relays = sentinel.relays
@@ -1774,3 +1786,34 @@ async def test_energy_scanning_partial(app):
     assert len(app._ezsp.startScan.mock_calls) == 6
     assert set(results.keys()) == {11, 13, 14, 15, 20, 25, 26}
     assert results == {c: map_rssi_to_energy(c) for c in [11, 13, 14, 15, 20, 25, 26]}
+
+
+async def test_connect_failure(
+    app: ControllerApplication, ezsp_mock: ezsp.EZSP
+) -> None:
+    """Test that a failure to connect propagates."""
+    ezsp_mock.startup_reset = AsyncMock(side_effect=OSError())
+    ezsp_mock.connect = AsyncMock()
+    app._ezsp = None
+
+    with patch("bellows.ezsp.EZSP", return_value=ezsp_mock):
+        with pytest.raises(OSError):
+            await app.connect()
+
+    assert app._ezsp is None
+
+    assert len(ezsp_mock.close.mock_calls) == 1
+
+
+async def test_repair_tclk_partner_ieee(app: ControllerApplication) -> None:
+    """Test that EZSP is reset after repairing TCLK."""
+    app._ensure_network_running = AsyncMock()
+    app.load_network_info = AsyncMock()
+
+    with patch(
+        "bellows.zigbee.repairs.fix_invalid_tclk_partner_ieee",
+        AsyncMock(return_value=True),
+    ):
+        await app.start_network()
+
+    assert len(app._ensure_network_running.mock_calls) == 2

bellows/ezsp/__init__.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 import asyncio
 import collections
 import contextlib
+import dataclasses
 import functools
 import logging
 import sys
@@ -20,6 +21,7 @@ import zigpy.config
 
 import bellows.config as conf
 from bellows.exception import EzspError, InvalidCommandError
+from bellows.ezsp.config import DEFAULT_CONFIG, RuntimeConfig, ValueConfig
 import bellows.types as t
 import bellows.uart
 
@@ -133,15 +135,14 @@ class EZSP:
     async def _probe(self) -> None:
         """Open port and try sending a command"""
         await self.connect(use_thread=False)
-        await self._startup_reset()
-        await self.version()
+        await self.startup_reset()
 
     @property
     def is_tcp_serial_port(self) -> bool:
         parsed_path = urllib.parse.urlparse(self._config[conf.CONF_DEVICE_PATH])
         return parsed_path.scheme == "socket"
 
-    async def _startup_reset(self):
+    async def startup_reset(self) -> None:
         """Start EZSP and reset the stack."""
         # `zigbeed` resets on startup
         if self.is_tcp_serial_port:
@@ -157,6 +158,8 @@ class EZSP:
         if not self.is_ezsp_running:
             await self.reset()
 
+        await self.version()
+
     @classmethod
     async def initialize(cls, zigpy_config: dict) -> EZSP:
         """Return initialized EZSP instance."""
@@ -164,12 +167,7 @@ class EZSP:
         await ezsp.connect(use_thread=zigpy_config[conf.CONF_USE_THREAD])
 
         try:
-            await ezsp._startup_reset()
-            await ezsp.version()
-            await ezsp._protocol.initialize(zigpy_config)
-
-            if zigpy_config[zigpy.config.CONF_SOURCE_ROUTING]:
-                await ezsp.set_source_routing()
+            await ezsp.startup_reset()
         except Exception:
             ezsp.close()
             raise
@@ -419,6 +417,20 @@ class EZSP:
         """Checks if the device EUI64 can be written any number of times."""
         return await self._get_nv3_restored_eui64_key() is not None
 
+    async def reset_custom_eui64(self) -> None:
+        """Reset the custom EUI64, if possible."""
+
+        nv3_eui64_key = await self._get_nv3_restored_eui64_key()
+        if nv3_eui64_key is None:
+            return
+
+        (status,) = await self.setTokenData(
+            nv3_eui64_key,
+            0,
+            t.LVBytes32(t.EmberEUI64.convert("FF:FF:FF:FF:FF:FF:FF:FF").serialize()),
+        )
+        assert status == t.EmberStatus.SUCCESS
+
     async def write_custom_eui64(
         self, ieee: t.EUI64, *, burn_into_userdata: bool = False
     ) -> None:
@@ -488,7 +500,9 @@ class EZSP:
         LOGGER.debug("Set concentrator type: %s", res)
         if res[0] != self.types.EmberStatus.SUCCESS:
             LOGGER.warning("Couldn't set concentrator type %s: %s", True, res)
-        await self._protocol.set_source_routing()
+
+        if self._ezsp_version >= 8:
+            await self.setSourceRouteDiscoveryMode(1)
 
     def start_ezsp(self):
         """Mark EZSP as running."""
@@ -512,3 +526,104 @@ class EZSP:
     def types(self):
         """Return EZSP types for this specific version."""
         return self._protocol.types
+
+    async def write_config(self, config: dict) -> None:
+        """Initialize EmberZNet Stack."""
+        config = self._protocol.SCHEMAS[conf.CONF_EZSP_CONFIG](config)
+
+        # Not all config will be present in every EZSP version so only use valid keys
+        ezsp_config = {}
+        ezsp_values = {}
+
+        for cfg in DEFAULT_CONFIG[self._ezsp_version]:
+            if isinstance(cfg, RuntimeConfig):
+                ezsp_config[cfg.config_id.name] = dataclasses.replace(
+                    cfg, config_id=self.types.EzspConfigId[cfg.config_id.name]
+                )
+            elif isinstance(cfg, ValueConfig):
+                ezsp_values[cfg.value_id.name] = dataclasses.replace(
+                    cfg, value_id=self.types.EzspValueId[cfg.value_id.name]
+                )
+
+        # Override the defaults with user-specified values (or `None` for deletions)
+        for name, value in config.items():
+            if value is None:
+                ezsp_config.pop(name)
+                continue
+
+            ezsp_config[name] = RuntimeConfig(
+                config_id=self.types.EzspConfigId[name],
+                value=value,
+            )
+
+        # Make sure CONFIG_PACKET_BUFFER_COUNT is always set last
+        if self.types.EzspConfigId.CONFIG_PACKET_BUFFER_COUNT.name in ezsp_config:
+            ezsp_config = {
+                **ezsp_config,
+                self.types.EzspConfigId.CONFIG_PACKET_BUFFER_COUNT.name: ezsp_config[
+                    self.types.EzspConfigId.CONFIG_PACKET_BUFFER_COUNT.name
+                ],
+            }
+
+        # First, set the values
+        for cfg in ezsp_values.values():
+            # XXX: A read failure does not mean the value is not writeable!
+            status, current_value = await self.getValue(cfg.value_id)
+
+            if status == self.types.EmberStatus.SUCCESS:
+                current_value, _ = type(cfg.value).deserialize(current_value)
+            else:
+                current_value = None
+
+            LOGGER.debug(
+                "Setting value %s = %s (old value %s)",
+                cfg.value_id.name,
+                cfg.value,
+                current_value,
+            )
+
+            (status,) = await self.setValue(cfg.value_id, cfg.value.serialize())
+
+            if status != self.types.EmberStatus.SUCCESS:
+                LOGGER.debug(
+                    "Could not set value %s = %s: %s",
+                    cfg.value_id.name,
+                    cfg.value,
+                    status,
+                )
+                continue
+
+        # Finally, set the config
+        for cfg in ezsp_config.values():
+            (status, current_value) = await self.getConfigurationValue(cfg.config_id)
+
+            # Only grow some config entries, all others should be set
+            if (
+                status == self.types.EmberStatus.SUCCESS
+                and cfg.minimum
+                and current_value >= cfg.value
+            ):
+                LOGGER.debug(
+                    "Current config %s = %s exceeds the default of %s, skipping",
+                    cfg.config_id.name,
+                    current_value,
+                    cfg.value,
+                )
+                continue
+
+            LOGGER.debug(
+                "Setting config %s = %s (old value %s)",
+                cfg.config_id.name,
+                cfg.value,
+                current_value,
+            )
+
+            (status,) = await self.setConfigurationValue(cfg.config_id, cfg.value)
+            if status != self.types.EmberStatus.SUCCESS:
+                LOGGER.debug(
+                    "Could not set config %s = %s: %s",
+                    cfg.config_id,
+                    cfg.value,
+                    status,
+                )
+                continue

bellows/ezsp/protocol.py
@@ -1,18 +1,17 @@
 import abc
 import asyncio
 import binascii
-import dataclasses
 import functools
 import logging
 import sys
-from typing import Any, Callable, Dict, Optional, Tuple
+from typing import Any, Callable, Tuple
 
 if sys.version_info[:2] < (3, 11):
     from async_timeout import timeout as asyncio_timeout  # pragma: no cover
 else:
     from asyncio import timeout as asyncio_timeout  # pragma: no cover
 
-from bellows.config import CONF_EZSP_CONFIG, CONF_EZSP_POLICIES
+from bellows.config import CONF_EZSP_POLICIES
 from bellows.exception import InvalidCommandError
 from bellows.typing import GatewayType
 
@@ -55,120 +54,6 @@ class ProtocolHandler(abc.ABC):
     async def pre_permit(self, time_s: int) -> None:
         """Schedule task before allowing new joins."""
 
-    async def initialize(self, zigpy_config: Dict) -> None:
-        """Initialize EmberZNet Stack."""
-
-        # Prevent circular import
-        from bellows.ezsp.config import DEFAULT_CONFIG, RuntimeConfig, ValueConfig
-
-        # Not all config will be present in every EZSP version so only use valid keys
-        ezsp_config = {}
-        ezsp_values = {}
-
-        for cfg in DEFAULT_CONFIG[self.VERSION]:
-            if isinstance(cfg, RuntimeConfig):
-                ezsp_config[cfg.config_id.name] = dataclasses.replace(
-                    cfg, config_id=self.types.EzspConfigId[cfg.config_id.name]
-                )
-            elif isinstance(cfg, ValueConfig):
-                ezsp_values[cfg.value_id.name] = dataclasses.replace(
-                    cfg, value_id=self.types.EzspValueId[cfg.value_id.name]
-                )
-
-        # Override the defaults with user-specified values (or `None` for deletions)
-        for name, value in self.SCHEMAS[CONF_EZSP_CONFIG](
-            zigpy_config[CONF_EZSP_CONFIG]
-        ).items():
-            if value is None:
-                ezsp_config.pop(name)
-                continue
-
-            ezsp_config[name] = RuntimeConfig(
-                config_id=self.types.EzspConfigId[name],
-                value=value,
-            )
-
-        # Make sure CONFIG_PACKET_BUFFER_COUNT is always set last
-        if self.types.EzspConfigId.CONFIG_PACKET_BUFFER_COUNT.name in ezsp_config:
-            ezsp_config = {
-                **ezsp_config,
-                self.types.EzspConfigId.CONFIG_PACKET_BUFFER_COUNT.name: ezsp_config[
-                    self.types.EzspConfigId.CONFIG_PACKET_BUFFER_COUNT.name
-                ],
-            }
-
-        # First, set the values
-        for cfg in ezsp_values.values():
-            # XXX: A read failure does not mean the value is not writeable!
-            status, current_value = await self.getValue(cfg.value_id)
-
-            if status == self.types.EmberStatus.SUCCESS:
-                current_value, _ = type(cfg.value).deserialize(current_value)
-            else:
-                current_value = None
-
-            LOGGER.debug(
-                "Setting value %s = %s (old value %s)",
-                cfg.value_id.name,
-                cfg.value,
-                current_value,
-            )
-
-            (status,) = await self.setValue(cfg.value_id, cfg.value.serialize())
-
-            if status != self.types.EmberStatus.SUCCESS:
-                LOGGER.debug(
-                    "Could not set value %s = %s: %s",
-                    cfg.value_id.name,
-                    cfg.value,
-                    status,
-                )
-                continue
-
-        # Finally, set the config
-        for cfg in ezsp_config.values():
-            (status, current_value) = await self.getConfigurationValue(cfg.config_id)
-
-            # Only grow some config entries, all others should be set
-            if (
-                status == self.types.EmberStatus.SUCCESS
-                and cfg.minimum
-                and current_value >= cfg.value
-            ):
-                LOGGER.debug(
-                    "Current config %s = %s exceeds the default of %s, skipping",
-                    cfg.config_id.name,
-                    current_value,
-                    cfg.value,
-                )
-                continue
-
-            LOGGER.debug(
-                "Setting config %s = %s (old value %s)",
-                cfg.config_id.name,
-                cfg.value,
-                current_value,
-            )
-
-            (status,) = await self.setConfigurationValue(cfg.config_id, cfg.value)
-            if status != self.types.EmberStatus.SUCCESS:
-                LOGGER.debug(
-                    "Could not set config %s = %s: %s",
-                    cfg.config_id,
-                    cfg.value,
-                    status,
-                )
-                continue
-
-    async def get_free_buffers(self) -> Optional[int]:
-        status, value = await self.getValue(self.types.EzspValueId.VALUE_FREE_BUFFERS)
-
-        if status != self.types.EzspStatus.SUCCESS:
-            LOGGER.debug("Couldn't get free buffers: %s", status)
-            return None
-
-        return int.from_bytes(value, byteorder="little")
-
     async def command(self, name, *args) -> Any:
         """Serialize command and send it."""
         LOGGER.debug("Send command %s: %s", name, args)
@@ -182,13 +67,10 @@ class ProtocolHandler(abc.ABC):
         async with asyncio_timeout(EZSP_CMD_TIMEOUT):
             return await future
 
-    async def set_source_routing(self) -> None:
-        """Enable source routing on NCP."""
-
-    async def update_policies(self, zigpy_config: dict) -> None:
+    async def update_policies(self, policy_config: dict) -> None:
         """Set up the policies for what the NCP should do."""
 
-        policies = self.SCHEMAS[CONF_EZSP_POLICIES](zigpy_config[CONF_EZSP_POLICIES])
+        policies = self.SCHEMAS[CONF_EZSP_POLICIES](policy_config)
         self.tc_policy = policies[self.types.EzspPolicyId.TRUST_CENTER_POLICY.name]
 
         for policy, value in policies.items():

bellows/ezsp/v10/__init__.py
@@ -1,19 +1,17 @@
 """"EZSP Protocol version 10 protocol handler."""
-import asyncio
 import logging
-from typing import Tuple
 
 import voluptuous
 
 import bellows.config
 
 from . import commands, config, types as v10_types
-from .. import protocol
+from ..v9 import EZSPv9
 
 LOGGER = logging.getLogger(__name__)
 
 
-class EZSPv10(protocol.ProtocolHandler):
+class EZSPv10(EZSPv9):
     """EZSP Version 10 Protocol version handler."""
 
     VERSION = 10
@@ -23,36 +21,3 @@ class EZSPv10(protocol.ProtocolHandler):
         bellows.config.CONF_EZSP_POLICIES: voluptuous.Schema(config.EZSP_POLICIES_SCH),
     }
     types = v10_types
-
-    def _ezsp_frame_tx(self, name: str) -> bytes:
-        """Serialize the frame id."""
-        cmd_id = self.COMMANDS[name][0]
-        hdr = [self._seq, 0x00, 0x01]
-        return bytes(hdr) + self.types.uint16_t(cmd_id).serialize()
-
-    def _ezsp_frame_rx(self, data: bytes) -> Tuple[int, int, bytes]:
-        """Handler for received data frame."""
-        seq, data = data[0], data[3:]
-        frame_id, data = self.types.uint16_t.deserialize(data)
-
-        return seq, frame_id, data
-
-    async def pre_permit(self, time_s: int) -> None:
-        """Temporarily change TC policy while allowing new joins."""
-        wild_card_ieee = v10_types.EmberEUI64([0xFF] * 8)
-        tc_link_key = v10_types.EmberKeyData(b"ZigBeeAlliance09")
-        await self.addTransientLinkKey(wild_card_ieee, tc_link_key)
-        await self.setPolicy(
-            v10_types.EzspPolicyId.TRUST_CENTER_POLICY,
-            v10_types.EzspDecisionBitmask.ALLOW_JOINS
-            | v10_types.EzspDecisionBitmask.ALLOW_UNSECURED_REJOINS,
-        )
-        await asyncio.sleep(time_s + 2)
-        await self.setPolicy(
-            v10_types.EzspPolicyId.TRUST_CENTER_POLICY,
-            self.tc_policy,
-        )
-
-    async def set_source_routing(self) -> None:
-        """Enable source routing on NCP."""
-        await self.setSourceRouteDiscoveryMode(1)

commit 4e8ad8c8f657829a6a4825b3fbe6d16b587265f2

Fix startup broken by #577 (#578)
* Include missing calls to `write_config` and `register_endpoints`

* Extend unit tests to ensure config is written and endpoints are added

bellows/zigbee/application.py
@@ -25,6 +25,7 @@ import zigpy.zdo.types as zdo_t
 
 import bellows
 from bellows.config import (
+    CONF_EZSP_CONFIG,
     CONF_EZSP_POLICIES,
     CONF_PARAM_MAX_WATCHDOG_FAILURES,
     CONF_USE_THREAD,
@@ -142,6 +143,8 @@ class ControllerApplication(zigpy.application.ControllerApplication):
             ezsp.close()
             raise
 
+        # Writing config is required here because network info can't be loaded otherwise
+        await ezsp.write_config(self.config[CONF_EZSP_CONFIG])
         self._ezsp = ezsp
 
     async def _ensure_network_running(self) -> bool:
@@ -169,6 +172,7 @@ class ControllerApplication(zigpy.application.ControllerApplication):
     async def start_network(self):
         ezsp = self._ezsp
 
+        await self.register_endpoints()
         await self._ensure_network_running()
 
         if await repairs.fix_invalid_tclk_partner_ieee(ezsp):

tests/test_application.py
@@ -64,6 +64,7 @@ def ezsp_mock(ieee):
     mock_ezsp.readCounters = AsyncMock(return_value=[[0] * 10])
     mock_ezsp.readAndClearCounters = AsyncMock(return_value=[[0] * 10])
     mock_ezsp.setPolicy = AsyncMock(return_value=[0])
+    mock_ezsp.addEndpoint = AsyncMock(return_value=[t.EmberStatus.SUCCESS])
     mock_ezsp.get_board_info = AsyncMock(
         return_value=("Mock Manufacturer", "Mock board", "Mock version")
     )
@@ -220,6 +221,9 @@ async def _test_startup(
 
     with p1, p2 as multicast_mock:
         await app.startup(auto_form=auto_form)
+
+    assert ezsp_mock.write_config.call_count == 1
+    assert ezsp_mock.addEndpoint.call_count >= 2
     assert multicast_mock.await_count == 1
 
 