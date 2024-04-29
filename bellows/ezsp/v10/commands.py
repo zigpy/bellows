2ba6889a919826a7192d7426fc39d3d82ec2a9d5
puddly <32534428+puddly@users.noreply.github.com>
2024-04-29 13:01:45 -0400

    Simplify v10 commands

--- bellows/ezsp/v10/commands.py
+++ bellows/ezsp/v10/commands.py
@@ -1,5 +1,17 @@
+from . import types as t
 from ..v9.commands import COMMANDS as COMMANDS_v9
 
 COMMANDS = {
     **COMMANDS_v9,
+    # Use the correct `EmberChildData` object with the extra field
+    "getChildData": (
+        0x004A,
+        (t.uint8_t,),
+        (t.EmberStatus, t.EmberChildData),
+    ),
+    "setChildData": (
+        0x00AC,
+        (t.uint8_t, t.EmberChildData),
+        (t.EmberStatus,),
+    ),
 }
