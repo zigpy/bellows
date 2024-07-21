48f85400b9b0f7ee27978fcb40b6ab1d62eba610
puddly <32534428+puddly@users.noreply.github.com>
2024-07-20 23:20:51 -0400

    `EmberCounterType`

--- bellows/ezsp/v5/types/named.py
+++ bellows/ezsp/v5/types/named.py
@@ -11,6 +11,7 @@ from bellows.types.named import (  # noqa: F401, F403
     EmberCertificateData,
     EmberConcentratorType,
     EmberConfigTxPowerMode,
+    EmberCounterType,
     EmberCurrentSecurityBitmask,
     EmberDeviceUpdate,
     EmberEventUnits,
@@ -60,106 +61,6 @@ from bellows.types.named import (  # noqa: F401, F403
     EzspZllNetworkOperation,
     KeyData,
 )
-
-
-class EmberCounterType(basic.enum8):
-    # Defines the events reported to the application by the
-    # readAndClearCounters command.
-
-    # The MAC received a broadcast.
-    COUNTER_MAC_RX_BROADCAST = 0
-    # The MAC transmitted a broadcast.
-    COUNTER_MAC_TX_BROADCAST = 1
-    # The MAC received a unicast.
-    COUNTER_MAC_RX_UNICAST = 2
-    # The MAC successfully transmitted a unicast.
-    COUNTER_MAC_TX_UNICAST_SUCCESS = 3
-    # The MAC retried a unicast.
-    COUNTER_MAC_TX_UNICAST_RETRY = 4
-    # The MAC unsuccessfully transmitted a unicast.
-    COUNTER_MAC_TX_UNICAST_FAILED = 5
-    # The APS layer received a data broadcast.
-    COUNTER_APS_DATA_RX_BROADCAST = 6
-    # The APS layer transmitted a data broadcast.
-    COUNTER_APS_DATA_TX_BROADCAST = 7
-    # The APS layer received a data unicast.
-    COUNTER_APS_DATA_RX_UNICAST = 8
-    # The APS layer successfully transmitted a data unicast.
-    COUNTER_APS_DATA_TX_UNICAST_SUCCESS = 9
-    # The APS layer retried a data unicast.
-    COUNTER_APS_DATA_TX_UNICAST_RETRY = 10
-    # The APS layer unsuccessfully transmitted a data unicast.
-    COUNTER_APS_DATA_TX_UNICAST_FAILED = 11
-    # The network layer successfully submitted a new route discovery to the MAC.
-    COUNTER_ROUTE_DISCOVERY_INITIATED = 12
-    # An entry was added to the neighbor table.
-    COUNTER_NEIGHBOR_ADDED = 13
-    # An entry was removed from the neighbor table.
-    COUNTER_NEIGHBOR_REMOVED = 14
-    # A neighbor table entry became stale because it had not been heard from.
-    COUNTER_NEIGHBOR_STALE = 15
-    # A node joined or rejoined to the network via this node.
-    COUNTER_JOIN_INDICATION = 16
-    # An entry was removed from the child table.
-    COUNTER_CHILD_REMOVED = 17
-    # EZSP-UART only. An overflow error occurred in the UART.
-    COUNTER_ASH_OVERFLOW_ERROR = 18
-    # EZSP-UART only. A framing error occurred in the UART.
-    COUNTER_ASH_FRAMING_ERROR = 19
-    # EZSP-UART only. An overrun error occurred in the UART.
-    COUNTER_ASH_OVERRUN_ERROR = 20
-    # A message was dropped at the network layer because the NWK frame counter
-    # was not higher than the last message seen from that source.
-    COUNTER_NWK_FRAME_COUNTER_FAILURE = 21
-    # A message was dropped at the APS layer because the APS frame counter was
-    # not higher than the last message seen from that source.
-    COUNTER_APS_FRAME_COUNTER_FAILURE = 22
-    # Utility counter for general debugging use.
-    COUNTER_UTILITY = 23
-    # A message was dropped at the APS layer because it had APS encryption but
-    # the key associated with the sender has not been authenticated, and thus
-    # the key is not authorized for use in APS data messages.
-    COUNTER_APS_LINK_KEY_NOT_AUTHORIZED = 24
-    # A NWK encrypted message was received but dropped because decryption
-    # failed.
-    COUNTER_NWK_DECRYPTION_FAILURE = 25
-    # An APS encrypted message was received but dropped because decryption
-    # failed.
-    COUNTER_APS_DECRYPTION_FAILURE = 26
-    # The number of times we failed to allocate a set of linked packet buffers.
-    # This doesn't necessarily mean that the packet buffer count was 0 at the
-    # time, but that the number requested was greater than the number free.
-    COUNTER_ALLOCATE_PACKET_BUFFER_FAILURE = 27
-    # The number of relayed unicast packets.
-    COUNTER_RELAYED_UNICAST = 28
-    # The number of times we dropped a packet due to reaching
-    # the preset PHY to MAC queue limit (emMaxPhyToMacQueueLength).
-    COUNTER_PHY_TO_MAC_QUEUE_LIMIT_REACHED = 29
-    # The number of times we dropped a packet due to the
-    # packet-validate library checking a packet and rejecting it
-    # due to length or other formatting problems.
-    COUNTER_PACKET_VALIDATE_LIBRARY_DROPPED_COUNT = 30
-    # The number of times the NWK retry queue is full and a
-    # new message failed to be added.
-    COUNTER_TYPE_NWK_RETRY_OVERFLOW = 31
-    # The number of times the PHY layer was unable to transmit
-    # due to a failed CCA.
-    COUNTER_PHY_CCA_FAIL_COUNT = 32
-    # The number of times a NWK broadcast was dropped because
-    # the broadcast table was full.
-    COUNTER_BROADCAST_TABLE_FULL = 33
-    # The number of low priority packet traffic arbitration requests.
-    COUNTER_PTA_LO_PRI_REQUESTED = 34
-    # The number of high priority packet traffic arbitration requests.
-    COUNTER_PTA_HI_PRI_REQUESTED = 35
-    # The number of low priority packet traffic arbitration requests denied.
-    COUNTER_PTA_LO_PRI_DENIED = 36
-    # The number of high priority packet traffic arbitration requests denied.
-    COUNTER_PTA_HI_PRI_DENIED = 37
-    # The number of aborted low priority packet traffic arbitration transmissions.
-    COUNTER_PTA_LO_PRI_TX_ABORTED = 38
-    # The number of aborted high priority packet traffic arbitration transmissions.
-    COUNTER_PTA_HI_PRI_TX_ABORTED = 39
 
 
 class EmberNetworkInitBitmask(basic.bitmap16):
