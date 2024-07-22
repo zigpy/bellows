b23cdacdffb16869df94280ef261991c12fb5b7d
puddly <32534428+puddly@users.noreply.github.com>
2024-07-22 11:08:06 -0400

    Migrate v7

--- bellows/ezsp/v7/commands.py
+++ bellows/ezsp/v7/commands.py
@@ -5,119 +5,233 @@ COMMANDS = {
     **COMMANDS_v6,
     "sendPanIdUpdate": (
         0x57,
-        (t.EmberPanId,),
-        (t.Bool,),
+        tuple(
+            {
+                "newPan": t.EmberPanId,
+            }.values()
+        ),
+        tuple(
+            {
+                "status": t.EmberStatus,
+            }.values()
+        ),
     ),
     "getTrueRandomEntropySource": (
         0x4F,
-        (),
-        (t.EmberEntropySource,),
+        tuple({}.values()),
+        tuple(
+            {
+                "entropySource": t.EmberEntropySource,
+            }.values()
+        ),
     ),
     "joinNetworkDirectly": (
         0x3B,
-        (t.EmberNodeType, t.EmberBeaconData, t.int8s, t.Bool),
-        (t.EmberStatus,),
+        tuple(
+            {
+                "localNodeType": t.EmberNodeType,
+                "beacon": t.EmberBeaconData,
+                "radioTxPower": t.int8s,
+                "clearBeaconsAfterNetworkUp": t.Bool,
+            }.values()
+        ),
+        tuple(
+            {
+                "status": t.EmberStatus,
+            }.values()
+        ),
     ),
     "getFirstBeacon": (
         0x3D,
-        (),
-        (t.EmberStatus, t.EmberBeaconIterator),
+        tuple({}.values()),
+        tuple(
+            {
+                "status": t.EmberStatus,
+                "beaconIterator": t.EmberBeaconIterator,
+            }.values()
+        ),
     ),
     "getNextBeacon": (
         0x04,
-        (),
-        (t.EmberStatus, t.EmberBeaconData),
+        tuple({}.values()),
+        tuple(
+            {
+                "status": t.EmberStatus,
+                "beaconData": t.EmberBeaconData,
+            }.values()
+        ),
     ),
     "getNumStoredBeacons": (
         0x08,
-        (),
-        (t.uint8_t,),
+        tuple({}.values()),
+        tuple(
+            {
+                "numStoredBeacons": t.uint8_t,
+            }.values()
+        ),
     ),
     "clearStoredBeacons": (
         0x3C,
-        (),
-        (),
+        tuple({}.values()),
+        tuple({}.values()),
     ),
     "unicastCurrentNetworkKey": (
         0x50,
-        (t.EmberNodeId, t.EUI64, t.EmberNodeId),
-        (t.EmberStatus,),
+        tuple(
+            {
+                "targetShort": t.EmberNodeId,
+                "targetLong": t.EUI64,
+                "parentShortId": t.EmberNodeId,
+            }.values()
+        ),
+        tuple(
+            {
+                "status": t.EmberStatus,
+            }.values()
+        ),
     ),
     "setMacPollCcaWaitTime": (
         0xF4,
-        (t.uint8_t,),
-        (),
+        tuple(
+            {
+                "waitBeforeRetryIntervalMs": t.uint8_t,
+            }.values()
+        ),
+        tuple({}.values()),
     ),
     "setBeaconClassificationParams": (
         0xEF,
-        (),
-        (t.EmberStatus, t.EmberBeaconClassificationParams),
+        tuple(
+            {
+                "param": t.EmberBeaconClassificationParams,
+            }.values()
+        ),
+        tuple(
+            {
+                "status": t.EmberStatus,
+            }.values()
+        ),
     ),
     "getBeaconClassificationParams": (
         0xF3,
-        (),
-        (t.EmberStatus, t.EmberBeaconClassificationParams),
+        tuple({}.values()),
+        tuple(
+            {
+                "status": t.EmberStatus,
+                "param": t.EmberBeaconClassificationParams,
+            }.values()
+        ),
     ),
     "updateTcLinkKey": (
         0x6C,
-        (t.uint8_t,),
-        (t.EmberStatus,),
+        tuple(
+            {
+                "maxAttempts": t.uint8_t,
+            }.values()
+        ),
+        tuple(
+            {
+                "status": t.EmberStatus,
+            }.values()
+        ),
     ),
     "getTransientKeyTableEntry": (
         0x6D,
-        (t.uint8_t,),
-        (t.EmberStatus, t.EmberTransientKeyDataV5),
+        tuple(
+            {
+                "index": t.uint8_t,
+            }.values()
+        ),
+        tuple(
+            {
+                "status": t.EmberStatus,
+                "keyStruct": t.EmberTransientKeyDataV5,
+            }.values()
+        ),
     ),
     "zllClearTokens": (
         0x25,
-        (),
-        (),
+        tuple({}.values()),
+        tuple({}.values()),
     ),
     "setParentClassificationEnabled": (
         0xE7,
-        (t.Bool,),
-        (),
+        tuple(
+            {
+                "enabled": t.Bool,
+            }.values()
+        ),
+        tuple({}.values()),
     ),
     "getParentClassificationEnabled": (
         0xF0,
-        (),
-        (t.Bool,),
+        tuple({}.values()),
+        tuple(
+            {
+                "enabled": t.Bool,
+            }.values()
+        ),
     ),
     "setLongUpTime": (
         0xE3,
-        (t.Bool,),
-        (),
+        tuple(
+            {
+                "hasLongUpTime": t.Bool,
+            }.values()
+        ),
+        tuple({}.values()),
     ),
     "setHubConnectivity": (
         0xE4,
-        (t.Bool,),
-        (),
+        tuple(
+            {
+                "connected": t.Bool,
+            }.values()
+        ),
+        tuple({}.values()),
     ),
     "isUpTimeLong": (
         0xE5,
-        (),
-        (t.Bool,),
+        tuple({}.values()),
+        tuple(
+            {
+                "hasLongUpTime": t.Bool,
+            }.values()
+        ),
     ),
     "isHubConnected": (
         0xE6,
-        (),
-        (t.Bool,),
+        tuple({}.values()),
+        tuple(
+            {
+                "connected": t.Bool,
+            }.values()
+        ),
     ),
     "gpSinkTableClearAll": (
         0xE2,
-        (),
-        (),
+        tuple({}.values()),
+        tuple({}.values()),
     ),
     "gpSinkTableInit": (
         0x70,
-        (),
-        (),
+        tuple({}.values()),
+        tuple({}.values()),
     ),
     # Changed commands
     "getChildData": (
         0x4A,
-        (t.uint8_t,),
-        (t.EmberStatus, t.EmberChildDataV7),
+        tuple(
+            {
+                "index": t.uint8_t,
+            }.values()
+        ),
+        tuple(
+            {
+                "status": t.EmberStatus,
+                "childData": t.EmberChildDataV7,
+            }.values()
+        ),
     ),
 }
 
