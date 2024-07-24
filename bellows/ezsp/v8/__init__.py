2ddaf5a9a0b9c1f6f00e07f4602578e75effeb38
puddly <32534428+puddly@users.noreply.github.com>
2024-07-24 11:12:35 -0400

    Use the correct kwarg when sending `setPolicy`

--- bellows/ezsp/v8/__init__.py
+++ bellows/ezsp/v8/__init__.py
@@ -41,7 +41,7 @@ class EZSPv8(EZSPv7):
         """Temporarily change TC policy while allowing new joins."""
         await super().pre_permit(time_s)
         await self.setPolicy(
-            valueId=t.EzspPolicyId.TRUST_CENTER_POLICY,
+            policyId=t.EzspPolicyId.TRUST_CENTER_POLICY,
             value=(
                 t.EzspDecisionBitmask.ALLOW_JOINS
                 | t.EzspDecisionBitmask.ALLOW_UNSECURED_REJOINS
@@ -49,6 +49,6 @@ class EZSPv8(EZSPv7):
         )
         await asyncio.sleep(time_s + 2)
         await self.setPolicy(
-            valueId=t.EzspPolicyId.TRUST_CENTER_POLICY,
+            policyId=t.EzspPolicyId.TRUST_CENTER_POLICY,
             value=self.tc_policy,
         )
