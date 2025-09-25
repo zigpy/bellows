a6c671d0760f9182c220e6a4debea6be04b273f3
puddly <32534428+puddly@users.noreply.github.com>
2025-09-25 12:04:37 -0400

    Fail gracefully when parsing invalid XNCP chip info response

--- bellows/exception.py
+++ bellows/exception.py
@@ -9,6 +9,12 @@ class InvalidCommandError(EzspError):
     pass
 
 
+class InvalidCommandPayload(InvalidCommandError):
+    def __init__(self, msg: str, raw_bytes: bytes) -> None:
+        super().__init__(msg)
+        self.raw_bytes = raw_bytes
+
+
 class ControllerError(ControllerException):
     pass
 
